8f932ac252ad2c7316ed03d5f4bb8430f8327e20
Eli Shagam <eli.shagam@gmail.com>
2022-03-24 10:05:00 +0200

    Remove Art from Gallery

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>Art Gallery</h1>
+      <h1>Gallery</h1>
       {/* <h2>Your Pictures</h2> */}
       {/* <p>------.</p> */}
     </div>
