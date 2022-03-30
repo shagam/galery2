7986e4efcd09963f966805dbfaa812cc2421bc33
Eli Shagam <eli.shagam@gmail.com>
2022-03-30 22:52:51 +0300

    Modal some improve

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -13,7 +13,19 @@ const Modal = ({ selectedDoc, setSelectedDoc }) => {
   return (
     <div className="backdrop" onClick={handleClick}>
       <img src={selectedDoc.fileUrl} alt="enlarged pic" />
-      <div>  fileName: {selectedDoc.fileName}  file_kb:  {selectedDoc.file_kb} fileType: {selectedDoc.fileType}</div>
+
+      <div className="modalFiedls">
+      <h4 className='text-center mb-1'>  fileName: {selectedDoc.fileName} </h4>
+      <h4 className='text-center mb-1'>  category: {selectedDoc.category} </h4>
+
+      <textarea rows="10" cols="70" name = "description"  readOnly
+          //  defaultValue={selectedDoc.description}
+          placeholder={selectedDoc.description} 
+           >
+      </textarea> 
+         
+
+      </div>
     </div>
   )
 }
