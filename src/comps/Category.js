e1474f2b718a4b8f0414a484d0254564ad9e951a
Eli Shagam <eli.shagam@gmail.com>
2022-05-10 11:56:22 +0300

    MobileContext

--- src/comps/Category.js
+++ src/comps/Category.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import MobileContext from './MobileContext';
+
 
 export const Category = (props) => {
   const [category, setCategory] = useState (props.category)
@@ -11,6 +13,14 @@ export const Category = (props) => {
     props.setCategory (e.target.value);
   }
 
+  const { userAgentMobile, isAndroid, isIPhone} = MobileContext();
+
+  var style = {};
+  if (! isAndroid)
+    style = {display: 'flex'}
+  else
+    style = {display: 'flex'}
+
 // , width: '3em', hieght: '3em'
   return (
     <>
@@ -18,10 +28,10 @@ export const Category = (props) => {
     <div  style={{display: 'flex', color: 'red'}}>
     category: &nbsp;&nbsp;&nbsp;
     {categoryList.map((category_) => (
-      <label key = {category_}  style={{display: 'flex'}} >
+      <div key = {category_}  style={style} >
         <input type='radio' style={{zoom: '150%'}} name='category' value={category_} checked={category === category_} onChange={exec} /> 
         <div>{category_}&nbsp;&nbsp;&nbsp;&nbsp;</div>
-      </label>
+      </div>
     ))}
   </div>
   </>
---
+++ src/comps/MobileContext.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+
+
+
+export const IsMobile = () => {
+  const [userAgent, setUserAgent] = useState("");
+  const [userAgentMobile, setUserAgentMobile] = useState(false);
+  const [isAndroid, setIsAndroid] = useState("");
+  const [isIPhone, setIsIphone] = useState("");
+
+  
+  useEffect (() => { 
+    //getGain();
+    checkMobile();
+     //getInfo();
+
+  }, [])
+
+  
+
+  const checkMobile = async () => {
+
+    // userAgent
+    const userAgent = navigator.userAgent;
+    setUserAgent(navigator.userAgent)
+    //if (/Android/i.test(navigator.userAgent))
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+      setUserAgentMobile(true);
+    } else {
+      setUserAgentMobile(false);
+      console.log("not mobile device");
+    }
+    
+    if (/Android/i.test(navigator.userAgent))
+      setIsAndroid(true)
+    else
+      setIsAndroid(false);
+
+      if (/iPhone/i.test(navigator.userAgent))
+      setIsIphone(true)
+    else
+      setIsIphone(false);
+
+  }
+
+  const value = {
+    userAgent,
+    userAgentMobile,
+    isAndroid,
+    isIPhone
+  }
+  return (value)
+
+}
+
+export default IsMobile;
+
+
