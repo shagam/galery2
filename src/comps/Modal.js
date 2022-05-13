69bc46628661efc4a9a6d7db992331855da3b7a8
Eli Shagam <eli.shagam@gmail.com>
2022-05-13 11:07:38 +0300

    Modal edit only for admin

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
 import EditDoc from '../table/EditDoc'
+import { useAuth } from '../contexts/AuthContext';
 
 const Modal = ({ selectedDoc, setSelectedDoc, getPictures, galery }) => {
   const [editDoc, setEditDoc] = useState();
+
+  const { admin } = useAuth();
 
   const handleClick = (e) => {
     if (e.target.classList.contains('backdrop')) {
@@ -14,15 +17,15 @@ const Modal = ({ selectedDoc, setSelectedDoc, getPictures, galery }) => {
 
     <div className="backdrop" onClick={handleClick}>
 
-      <img src={selectedDoc.fileUrl} alt="enlarged pic" />
+      {!editDoc && <img src={selectedDoc.fileUrl} alt="enlarged pic" />}
 
       <div style={{display:'flex'}}>
         <button type="button" style= {{'fontSize':'1.8vw' }} onClick={()=>setSelectedDoc (null)}>close</button>
-        <button type="button" style= {{'fontSize':'1.8vw' }} onClick={()=>setEditDoc (selectedDoc)}>edit</button>
+        {admin && <button type="button" style= {{'fontSize':'1.8vw' }} onClick={()=>setEditDoc (selectedDoc)}>edit</button>}
         <h4 className='text-center mb-1' style= {{color:'magenta','fontSize':'1.8vw' }} >  &nbsp; <strong>  {selectedDoc.fileName}</strong>  &nbsp;   </h4>
         <h4  style= {{'fontSize':'1.8vw' }}> {selectedDoc.category} &nbsp; {selectedDoc.technique}  &nbsp; {selectedDoc.size}  &nbsp; &nbsp; {selectedDoc.year}</h4>
       </div>
-
+      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>      <hr/>
 
       {editDoc && <EditDoc editDoc={selectedDoc} getPictures = {getPictures} setEditDoc={setEditDoc} gallery = {galery}/>}
 
