135cf9a7fa041df2faf4546480a2f4f48ddd24b3
Eli Shagam <eli.shagam@gmail.com>
2022-05-13 17:47:07 +0300

    Temp save layout of editDoc

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -20,16 +20,18 @@ const Modal = ({ selectedDoc, setSelectedDoc, getPictures, gallery }) => {
       { !editDoc && <div>
       
         <img src={selectedDoc.fileUrl} alt="enlarged pic" />
-        <hr/>  
+     
         <div style={{display:'flex'}}>
           <button type="button" style= {{'fontSize':'1.8vw' }} onClick={()=>setSelectedDoc (null)}>close</button>
           {admin && <button type="button" style= {{'fontSize':'1.8vw' }} onClick={()=>setEditDoc (selectedDoc)}>edit</button>}
           <h4 className='text-center mb-1' style= {{color:'magenta','fontSize':'1.8vw' }} >  &nbsp; <strong>  {selectedDoc.fileName}</strong>  &nbsp;   </h4>
           <h4  style= {{'fontSize':'1.8vw' }}> {selectedDoc.category} &nbsp; {selectedDoc.technique}  &nbsp; {selectedDoc.size}  &nbsp; &nbsp; {selectedDoc.year}</h4>
-          <hr/>               
+             
         </div>
         {/* <hr/>         <hr/>      <hr/> */}
         </div>}
+        <hr/>   <hr/>   <hr/>   <hr/>   
+        
         {editDoc && <EditDoc editDoc={selectedDoc} getPictures = {getPictures} setEditDoc={setEditDoc} gallery = {gallery}/>}
 
 
