98305ea5a46c1d931852fdf6e9773fbafa5ea983
Eli Shagam <eli.shagam@gmail.com>
2022-09-07 18:23:22 +0300

    modified:   src/comps/EditDoc.js 	modified:   src/comps/ImageGrid.js Add category Fruits

--- src/comps/EditDoc.js
+++ src/comps/EditDoc.js
@@ -34,7 +34,7 @@ const  EditDoc = (props) => {
   function abort () {
     props.setEditDoc(undefined);
   }
-  const categoryList =['Other','Landscape', 'Building', 'Nature','Fabrique','stickers']
+  const categoryList =['Other','Landscape', 'Building', 'Nature','Fabrique','stickers','fruits']
   if (category === '')
     setCategory (categoryList[0])
 
--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -126,7 +126,7 @@ const ImageGrid = (props) => {
     /* justify-content: start; */
   }
 
-  const categoryList =['all', 'Landscape', 'Building', 'Nature','Fabrique','stickers','Other']
+  const categoryList =['all', 'Landscape', 'Building', 'Nature','Fabrique','stickers','fruits', 'Other']
 
   const emailFontSize = userAgentMobile ? '3vw' : '1.4vw'
 
