c9fcc77b482a54b20848ea393efd3c51f4d532e8
Eli Shagam <eli.shagam@gmail.com>
2022-09-11 16:22:41 +0300

    modified:   src/comps/Represent.js  Remove unused, category font bigger

--- src/comps/Represent.js
+++ src/comps/Represent.js
@@ -24,19 +24,16 @@ export function Represent (props) {
         repDocs_.push(props.docs[i])
       }
     }
-    var str = '';
-    for (let i = 0; i < repDocs_.length; i++)
-      str += repDocs_[i].fileName + ' (' + repDocs_[i].category + ')  ';
+    // var str = '';
+    // for (let i = 0; i < repDocs_.length; i++)
+    //   str += repDocs_[i].fileName + ' (' + repDocs_[i].category + ')  ';
+    // console.log (str)
 
-    console.log (repsArray, str)
+    console.log (repsArray)
     if (repDocs_.length > 0)
       setRepDocs(repDocs_)
     
   }
-
-  // useEffect (() => { // eslint-disable-line
-  //   getPictures ();  // eslint-disable-line
-  // }, []);  // eslint-disable-line
 
   useEffect (() => {
   if (props.docs && props.docs.length > 0 && (! repDocs || repDocs.length === 0)) {
@@ -52,7 +49,7 @@ export function Represent (props) {
     /* margin: 20px auto; */
     display: 'grid',
     // 'grid-template-columns': '1fr 1fr 1fr',
-    'gridTemplateColumns':  '1fr 1fr 1fr 1fr' ,
+    'gridTemplateColumns':  '1fr 1fr 1fr' ,
     'gridGap': '10px'
     /* justify-content: start; */
   }
@@ -61,18 +58,19 @@ export function Represent (props) {
     <div style={img_grid}>
       {repDocs && repDocs.map(doc => {
           const {fileName, fileType, fileUrl, category, technique, size, year, title} = doc;
-          (size === undefined || year === undefined) && console.log (size,year)
+
           return <div key={fileName}>        
             <div className="img-wrap" 
               onClick={() => props.setCategory(doc.category)} >
+
               {(fileType === 'image/jpeg' || fileType === 'image/png') &&
                 <img src={fileUrl} alt={fileName} />}
               {doc.fileType === 'application/pdf' &&
-                <iframe src={doc.fileUrl} title={fileName} />   }
+                <iframe src={fileUrl} title={fileName} />}
+
             </div>
-            <div  style_={{display:'flex'}}>
-              <div style={{color:'magenta', 'fontSize':'1.8vw'}}> {title}  &nbsp;  </div>
-              <div style= {{'fontSize':'1.6vw' }}> ({category}, {technique})</div>
+            <div>
+              <div style= {{'fontSize':'5vw' }}> {category}</div>
             </div>
           </div> 
         })}
