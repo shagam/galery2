9d514cf8877ceedf82b37aa09bbdc43eccd74d86
Eli Shagam <eli.shagam@gmail.com>
2022-09-19 23:48:00 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/comps/Contact.js 	modified:   src/index.css   First working Contact.js

--- src/comps/Contact.js
+++ src/comps/Contact.js
@@ -3,81 +3,112 @@ import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext';
 
+import emailjs from '@emailjs/browser';
 
+export default function ContactUs (props)  {
 
-export default function Login (props)  {
-
-  const name = useRef()
+  const nameRef = useRef()
   const emailRef = useRef();
   // const emailConfirmRef = useRef();
   const pictureRef = useRef();
   const commentRef = useState();
+
+  const form = useRef();
 
   const [error, setError] = useState ('');
   const [loading, setLoading] = useState(false);
 
 
   const navigate = useNavigate();
+  
 
-
-  async function handleSubmit (e) {
+   function sendEmail (e) {
     e.preventDefault();
     // if (emailRef.current.value !== emailConfirmRef.current.value) {
     //   return setError ('Passwords do not match')
     // } 
 
-    try {
-      setError('');
-      setLoading(true);
-      // await login (emailRef.current.value, passwordRef.current.value)
-      navigate ('/')
+      console.log (form.current)
+      emailjs.sendForm('service_hckn29m', 'template_g6rsdnw', form.current, 'HNB274P5Zug2myTir')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+    };
 
-    } catch (e) {setError (e.message)}
-    setLoading (false);
-  }
+  //   try {
+  //     setError('');
+  //     setLoading(true);
+  //     // await login (emailRef.current.value, passwordRef.current.value)
+  //     navigate ('/')
+
+  //   } catch (e) {setError (e.message)}
+  //   setLoading (false);
+  // }
 
   return (
+    <section>
+      <div className='container'>
+        <h2 className="--text-center"> Contact Us </h2>
+        <form ref={form} onSubmit={sendEmail}
+          className="--form-control --card --flex-center --dir-column">
+          <label>Name</label>
+          <input type="text" name="user_name" placeholder="Full Name" />
+          <label>Email</label>
+          <input type="email" name="user_email" placeholder="Email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </section>
+  );
 
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className='text-center mb-4'> Contact info</h2>
 
-          {error && <Alert variant="danger"> {error} </Alert>}
-          <hr/> 
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="name">
-              <Form.Label>name</Form.Label>
-              <Form.Control type="text" ref= {emailRef} required />
-            </Form.Group>
+  // return (
 
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref= {emailRef} required />
-            </Form.Group>
+  //   <>
+  //     <Card>
+  //       <Card.Body>
+  //         <h2 className='text-center mb-4'> Contact info</h2>
 
-            {/* <Form.Group id="confirmEmail">
-              <Form.Label>Confrm email</Form.Label>
-              <Form.Control type="email" ref= {emailConfirmRef} required />
-            </Form.Group>
- */}
-            <Form.Group id="pictureName">
-              <Form.Label>picture name</Form.Label>
-              <Form.Control type="text" ref= {pictureRef} required />
-            </Form.Group>
+  //         {error && <Alert variant="danger"> {error} </Alert>}
+  //         <hr/> 
+  //         <Form onSubmit={sendEmail}>
+  //           <Form.Group id="name">
+  //             <Form.Label>name</Form.Label>
+  //             <Form.Control type="text" ref= {emailRef} required />
+  //           </Form.Group>
 
-            <Form.Group id="comment">
-              <Form.Label>Comment</Form.Label>
-              <Form.Control type="text" ref= {commentRef} />
-            </Form.Group>
-            <div> 
-           </div>
-           <hr/>
-            <Button disabled={loading} className="w-100" type="submit"> Submit </Button>
-          </Form>
-        </Card.Body>
-      </Card>
+  //           <Form.Group id="email">
+  //             <Form.Label>Email</Form.Label>
+  //             <Form.Control type="email" ref= {emailRef} required />
+  //           </Form.Group>
 
-    </>
-  )
+  //           {/* <Form.Group id="confirmEmail">
+  //             <Form.Label>Confrm email</Form.Label>
+  //             <Form.Control type="email" ref= {emailConfirmRef} required />
+  //           </Form.Group>
+  //         */}
+  //           <Form.Group id="pictureName">
+  //             <Form.Label>picture name</Form.Label>
+  //             <Form.Control type="text" ref= {pictureRef} required />
+  //           </Form.Group>
+
+  //           <Form.Group id="comment">
+  //             <Form.Label>Comment</Form.Label>
+  //             <Form.Control type="text" ref= {commentRef} />
+  //           </Form.Group>
+  //           <div> 
+  //          </div>
+  //          <hr/>
+  //           <Button disabled={loading} className="w-100" type="submit"> Send message </Button>
+  //         </Form>
+  //       </Card.Body>
+  //     </Card>
+
+  //   </>
+  // )
 }
